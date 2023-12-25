8010643c4914e21a946c6dc22c701a947dbf9591
QuananhNguyen-04 <sherlingfordk@gmail.com>
2023-12-25 20:54:13 +0700

    Adding Features stage

--- base.js
+++ base.js
@@ -1,15 +1,28 @@
 const { app, BrowserWindow } = require('electron')
 
+const path = require('node:path') 	// add dependencies for upload
+									// view https://www.electronjs.org/docs/latest/tutorial/tutorial-preload
 
 const createWindow = () => {
 	const win = new BrowserWindow ({
 		width: 800,
-		height: 600
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
 	});
 
 	win.loadFile("./index.html");
 }
 
 app.whenReady().then(() => {
-	createWindow()
+	createWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length() === 0) createWindow();
+	})
+})
+
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
 })

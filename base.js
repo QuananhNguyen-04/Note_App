7293da5efb210fa0ab98739805130d7843fd345f
QuananhNguyen-04 <sherlingfordk@gmail.com>
2023-12-29 02:25:39 +0700

    Switching between 2 pages

--- base.js
+++ base.js
@@ -6,10 +6,22 @@ const path = require('node:path'); 	// add dependencies for upload
 // view https://www.electronjs.org/docs/latest/tutorial/tutorial-preload
 const isDev = process.env.NODE_ENV !== 'development'
 
+ipcMain.handle('saveFile', saveFile);
+ipcMain.handle('open-child-page', openWritingChild);
+ipcMain.handle('close-write', () => { 
+	mainWin.show();
+	winChild.close();
+	// console.log(mainWin.show)
+})
+
+let mainWin, winChild;
+var center = null;
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: isDev ? 1000 : 400,
-		height: isDev ? 700 : 500,
+	mainWin = new BrowserWindow({
+		x: center? center[0]: null,
+		y: center? center[1]: null,
+		width: isDev ? 500 : 400,
+		height: 300,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			nodeIntegration: false,
@@ -18,19 +30,65 @@ const createWindow = () => {
 		}
 	});
 	if (isDev) {
-		win.webContents.openDevTools();
+		// mainWin.webContents.openDevTools();
 	}
-	ipcMain.handle('saveFile', saveFile);
-	// ipcMain.handle('openPage', createSub);
-	win.loadFile("./mainPage.html");
+	// ipcMain.handle('saveFile', saveFile);
+	// ipcMain.handle('open-child-page', openWritingChild);
+	mainWin.loadFile("./mainPage.html");
+	center = mainWin.getPosition();
+	console.log(center)
 }
 
-// function createSub() {
-// 	// if (!dir) {
-// 	// 	return;
-// 	// }
-// 	window.location.assign('./writing_features/index.html')
-// }
+function openWritingChild(event) {
+	// if (!dir) {
+	// 	return;
+	// }
+	console.log("in basejs");
+	winChild = new BrowserWindow({
+		parent: mainWin,
+		width: isDev ? 600 : 400,
+		height: 500,
+		x: center? center[0] - (isDev? 100: 100): null,
+		y: center? center[1] - 200: null,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: false,
+			nodeIntegrationInWorker: true,
+			contextIsolation: isDev ? true : false,
+		}
+	});
+	// ipcMain.handle()
+	event = event || window.event;
+	event.preventDefault();
+	mainWin.setAlwaysOnTop(true, 'screen-saver');
+	mainWin.hide();
+	winChild.loadFile("./writing_features/index.html");
+	winChild.show();
+	winChild.on("closed", () => {
+		mainWin.show();
+		console.log(mainWin.show)
+	})
+	// winChild.on("closed", () => {
+	// 	if (mainWin.on("hide", _) 
+	// 	&& BrowserWindow.getAllWindows().length() === 1 
+	// 	&& process.platform !== 'darwin') 
+
+	// 		app.quit();
+	// })
+	// window.location.assign('./writing_features/index.html')
+}
+
+
+app.whenReady().then(() => {
+	createWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length() === 0) createWindow();
+	});
+
+	mainWin.webContents.on('did-finish-load', () => {
+		mainWin.webContents.send('app-ready');
+	});
+})
 
 function saveFile(_, { title, data }) {
 	if (!title || !data) {
@@ -45,14 +103,6 @@ function saveFile(_, { title, data }) {
 	writeFileSync(datapath, data);
 	return { success: true };
 }
-
-app.whenReady().then(() => {
-	createWindow();
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length() === 0) createWindow();
-	})
-})
-
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') app.quit()
 })
--- preload.js
+++ preload.js
@@ -1,22 +1,38 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions', {
-	node: () => process.versions.node,
-	chrome: () => process.versions.chrome,
-	electron: () => process.versions.electron,
-	// we can also expose variables, not just functions
-})
-
-contextBridge.exposeInMainWorld('dataFeature', {
-	createFile: (title, data) => {
-		// console.log("in preload.js")
-		// console.log(title, data);
-		return ipcRenderer.invoke('saveFile', { title, data })
-	}
+contextBridge.exposeInMainWorld(
+	'versions', {
+		node: () => process.versions.node,
+		chrome: () => process.versions.chrome,
+		electron: () => process.versions.electron,
+		// we can also expose variables, not just functions
 });
-
-// contextBridge.exposeInMainWorld('openPage', {
-// 	open: () => {
-// 		return ipcRenderer.invoke('openPage');
-// 	}
-// });
+contextBridge.exposeInMainWorld(
+	'dataFeature', {
+		createFile: (title, data) => {
+			// console.log("in preload.js")
+			// console.log(title, data);
+			return ipcRenderer.invoke('saveFile', { title, data })
+		}
+});
+contextBridge.exposeInMainWorld(
+	'openPage', {
+		openWriting: () => {
+			console.log("in preload")
+			ipcRenderer.on('app-ready', ipcRenderer.invoke('open-child-page'));
+		},
+		openDrawing: () => {
+			console.log("in preload")
+		}
+});
+contextBridge.exposeInMainWorld(
+	'closePage', {
+		closeWrite: () => {
+			console.log("in preload")
+			// ipcRenderer.on('app-ready', ipcRenderer.invoke('open-child-page'));
+			ipcRenderer.invoke("close-write")
+		},
+		openDrawing: () => {
+			console.log("in preload")
+		}
+});
